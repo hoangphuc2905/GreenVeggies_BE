8943dd7d3ca47091b0745b6d15e0cdd4b3463cd5
Pkkoi1 <khoinhokboddy@gmail.com>
2025-05-12 16:29:57 +0700

    feat: update CORS methods to include PATCH across all services; enhance express configuration

--- AuthService/swagger.js
+++ AuthService/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:8000/api/auth",
+        url: "http://localhost:8000",
         description: "Local server",
       },
     ],
--- PaymentService/index.js
+++ PaymentService/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
